93e18ec990dc044e51157bd89816e993a3aa9867
Ben Livengood <ben@doc.ai>
2022-05-11 12:13:02 -0700

    Set up for Anthem Bamboo build

---
+++ ToniqDeployAuth.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+# set expandtab tabstop=2 shiftwidth=2 softtabstop=2 */
+
+##############################################################
+#
+# DevOps agent post-Bamboo script for toniq-ambassador-auth-oidc
+#   (only root user can run docker)
+#
+#  Builds Notebook and Hive  docker images and publishes to ECRs
+#
+#  Input environment values expected
+#   - ECR_REGISTRY # ECR URL ie. 481935479534.dkr.ecr.us-east-1.amazonaws.com
+#   - BUILD_AWS_ENV # ie. sit [dev, sit, prod]
+#
+##############################################################
+
+set -o errexit
+
+##############################################################
+# Utils
+##############################################################
+trim() {
+    local var="$*"
+    # remove leading whitespace characters
+    var="${var#"${var%%[![:space:]]*}"}"
+    # remove trailing whitespace characters
+    var="${var%"${var##*[![:space:]]}"}"   
+    printf '%s' "$var"
+}
+
+##############################################################
+# ENV check
+##############################################################
+
+echo "Checking to ensure required ENV exists..."
+
+requiredEnvs=(ECR_REGISTRY BUILD_AWS_ENV)
+for env in ${requiredEnvs[@]}; do
+  if [[ -z "${!env}" ]]; then
+    echo "$env value required"
+    exit 1
+  fi
+done
+
+# trim spaces
+ECR_REGISTRY="$(trim "${ECR_REGISTRY}")"
+BUILD_AWS_ENV="$(trim "${BUILD_AWS_ENV}")"
+
+echo "ECR_REGISTRY: ${ECR_REGISTRY}"
+echo "BUILD_AWS_ENV: ${BUILD_AWS_ENV}"
+
+echo "Get the latest docker cred via aws..."
+aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin ${ECR_REGISTRY}
+
+echo "Get dockerhub.com credentials from secret"
+aws secretsmanager get-secret-value --secret-id /${BUILD_AWS_ENV}/toniq-apiv1/dockercred/docai --region us-east-1 | grep SecretString | cut -f 4 -d '"' | sudo docker login --username anthembuildacct --password-stdin
+
+PYPI_PASSWORD=`aws secretsmanager get-secret-value --secret-id /${BUILD_AWS_ENV}/toniq-apiv1/pypi-password/docai --region us-east-1 | grep SecretString | cut -f 4 -d '"'`
+
+##############################################################
+# HUB
+##############################################################
+echo "Build and publish to ECR process for HUB..."
+
+# Prefix must be the partial prefix for all 
+ECR_PREFIX=${ECR_REGISTRY}/antm-docai-${BUILD_AWS_ENV}-ecr-
+
+echo "ECR_PREFIX: ${ECR_PREFIX}"
+sudo ./generic_build.sh ${ECR_PREFIX} ${PYPI_PASSWORD}
--- build.sh
+++ build.sh
@@ -3,10 +3,23 @@
 # this script will default to the Toniq GCR url but can be called and passed any registry/repo url like so:
 # ./build.sh 12345.dkr.ecr.us-east-1.amazonaws.com/toniq
 
-BASE_REPOSITORY_URL=${1:-us.gcr.io/terraform-254700/toniq}
+BASE_REPOSITORY_URL=${1:-us.gcr.io/terraform-254700/toniq/}
 VERSION_TAG=`cat VERSION | tr -d '\n'`
+ECR_REPOSITORY_URL=484285707936.dkr.ecr.us-west-2.amazonaws.com/antm-docai-dev-ecr-toniq-ambassador-auth-oidc:$VERSION_TAG
 
-docker build -t $BASE_REPOSITORY_URL/ambassador-auth-oidc:$VERSION_TAG -t $BASE_REPOSITORY_URL/ambassador-auth-oidc:latest .
+docker build -t ${BASE_REPOSITORY_URL}toniq-ambassador-auth-oidc:$VERSION_TAG -t ${BASE_REPOSITORY_URL}toniq-ambassador-auth-oidc:latest -t $ECR_REPOSITORY_URL .
 
-docker push $BASE_REPOSITORY_URL/ambassador-auth-oidc:$VERSION_TAG
-docker push $BASE_REPOSITORY_URL/ambassador-auth-oidc:latest
+docker push ${BASE_REPOSITORY_URL}toniq-ambassador-auth-oidc:$VERSION_TAG
+docker push ${BASE_REPOSITORY_URL}toniq-ambassador-auth-oidc:latest
+
+if [ -d .git ]
+   then
+       echo "Getting ECR credentials"
+       mkdir -p ~/.aws
+       gcloud secrets versions access --project terraform-254700 2 --secret aws-ecr-user > ~/.aws/credentials || echo "failed"
+
+       echo "Getting docker credentials"
+       aws ecr get-login-password --profile default  --region us-west-2 | docker login --username AWS --password-stdin 484285707936.dkr.ecr.us-west-2.amazonaws.com/antm-docai-dev-ecr-toniq-mlflow || echo "failed"
+       echo "Pushing image to ECR"
+       docker push ${ECR_REPOSITORY_URL}
+fi
